160ab53c2bc03bbde808b719c11ee79a8cb7a456
sdv22 <sdv22@live.ru>
2020-05-23 19:44:35 +0300

    Maxbitpool 53 to satisfy SBC offload
    Change-Id: I9caa2341cc191dfebb532cecfb927389fdbc2cd6

--- stack/a2dp/a2dp_sbc.cc
+++ stack/a2dp/a2dp_sbc.cc
@@ -40,7 +40,7 @@
 #include "osi/include/osi.h"
 #include "osi/include/properties.h"
 
-#define A2DP_SBC_MAX_BITPOOL 83
+#define A2DP_SBC_MAX_BITPOOL 53
 
 /* data type for the SBC Codec Information Element */
 typedef struct {
